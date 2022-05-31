85df823b535d44a1a471ad95ef77fdfaf62ce76c
csteitz01 <csteitz01@bellarmine.edu>
2022-05-31 12:46:55 -0400

    Finally got walls to work
    NEVER USE IF STATEMENTS WITHOUT CURLY BRACES. I modified my if statements to make the walls appear if that wall isn't connected to a room, but originally didn't have curly brackets so it ran regardless if conditions were met. I also added 2d box colliders so player can only go through doors now instead of the whole wall

--- WKUOMUS/Assets/Scripts/DungeonGeneration/Room.cs
+++ WKUOMUS/Assets/Scripts/DungeonGeneration/Room.cs
@@ -72,27 +72,35 @@ public class Room : MonoBehaviour
             {
                 case Door.DoorType.right:
                     if(GetRight() == null)
+                    {
                         //door.gameObject.SetActive(false);
                         door.transform.GetChild(0).gameObject.SetActive(false);
                         door.transform.GetChild(1).gameObject.SetActive(true);
+                    }
                 break;
                 case Door.DoorType.left:
                     if(GetLeft() == null)
+                    {
                         //door.gameObject.SetActive(false);
                         door.transform.GetChild(0).gameObject.SetActive(false);
-                        door.transform.GetChild(1).gameObject.SetActive(false);
+                        door.transform.GetChild(1).gameObject.SetActive(true);
+                    }
                 break;
                 case Door.DoorType.top:
                     if(GetTop() == null)
+                    {
                         //door.gameObject.SetActive(false);
                         door.transform.GetChild(0).gameObject.SetActive(false);
-                        door.transform.GetChild(1).gameObject.SetActive(false);
+                        door.transform.GetChild(1).gameObject.SetActive(true);
+                    }
                 break;
                 case Door.DoorType.bottom:
                     if(GetBottom() == null)
+                    {
                         //door.gameObject.SetActive(false);
                         door.transform.GetChild(0).gameObject.SetActive(false);
-                        door.transform.GetChild(1).gameObject.SetActive(false);
+                        door.transform.GetChild(1).gameObject.SetActive(true);
+                    }
                 break;
             }
         }
